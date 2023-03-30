710391ad87f834727382c4330f768ac8c95df832
tiagopinhotx <tiago.pinhotx@outlook.com>
2023-03-30 18:23:04 -0300

    Elixir: Collatz Conjecture Solution

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -6,6 +6,20 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) do
+  def calc(input) when input > 0 and not is_binary(input) do
+    get_steps(input)
   end
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  defp get_steps(input, times \\ 0)
+  defp get_steps(input, steps) when input == 1, do: steps
+  defp get_steps(input, steps) when rem(input, 2) == 0, do:
+  input
+  |> divide_even()
+  |> get_steps(steps + 1)
+
+
+  defp get_steps(input, steps), do: multiply_and_sum_odd(input) |> get_steps(steps + 1)
+
+  defp divide_even(even), do: div(even, 2)
+  defp multiply_and_sum_odd(odd), do: odd * 3 + 1
 end
